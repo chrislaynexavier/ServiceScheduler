c3eb3a963289990185b36ecc88ceb463077ff9c5
chrislaynexavier <chrislayne_pereira@hotmail.com>
2023-03-27 19:47:29 -0300

    Test client

---
+++ test/Client.spec.ts
@@ -0,0 +1,24 @@
+import { Client } from "../src/domain/entity/Client"
+
+describe('User', () => {
+    let newClient: Client
+    let name = "Chris"
+    let CPF = "000.000.000-09"
+    let birthDate = new Date("02/31/2000")
+
+    beforeEach(() => {
+      newClient = new Client(name,CPF,birthDate)
+    })
+
+    it('should have a client with correct name', () => {
+      expect(newClient.name).toBe(name)
+    })
+
+    it('should have a user with correct CPF', () => {
+      expect(newClient.CPF).toBe(CPF)
+    })
+      
+    it('should have a user with correct birthDate', () => {
+      expect(newClient.birthDate).toBe(birthDate)
+    })
+})
